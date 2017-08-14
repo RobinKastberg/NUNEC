6410a3baa316e7a5492692929e776319ae5b8481
kastberg <kastberg@gmail.com>
2017-08-14 22:14:04 +0200

    Linux port

---
+++ NUNEC/NUNEC/win.c
@@ -0,0 +1,165 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <windows.h>
+#pragma comment (lib, "opengl32.lib")
+#pragma comment (lib, "glew\\glew32s.lib")
+HWND wnd;
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
+void swap()
+{
+		SwapBuffers(GetDC(wnd));
+}
+int WinMain(__in HINSTANCE hInstance, __in_opt HINSTANCE hPrevInstance, __in_opt LPSTR lpCmdLine, __in int nShowCmd)
+{
+	AllocConsole();
+	freopen("CONIN$", "r", stdin);
+	freopen("CONOUT$", "w", stdout);
+	freopen("CONOUT$", "w", stderr);
+
+	MSG msg = { 0 };
+	WNDCLASS wc = { 0 };
+	wc.lpfnWndProc = WndProc;
+	wc.hInstance = hInstance;
+	wc.hbrBackground = (HBRUSH)(COLOR_BACKGROUND);
+	wc.lpszClassName = L"oglversionchecksample";
+	wc.style = CS_OWNDC;
+	if (!RegisterClass(&wc))
+		return 1;
+	wnd = CreateWindowW(wc.lpszClassName, L"openglversioncheck", WS_OVERLAPPEDWINDOW | WS_VISIBLE, 0, 0, 640, 480, 0, 0, hInstance, 0);
+	
+	while (1)
+	{
+		while (PeekMessage(&msg, 0, 0, 0, PM_REMOVE))
+		{
+			TranslateMessage(&msg);             // Translate The Message
+			DispatchMessage(&msg);              // Dispatch The Message
+		}
+
+		if (msg.message == WM_QUIT)
+		{
+			break;
+		}
+
+		// Do update, rendering and all the real game loop stuff
+		draw();
+
+	}
+
+	return 0;
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	switch (message)
+	{
+	case WM_CREATE:
+	{
+		PIXELFORMATDESCRIPTOR pfd =
+		{
+			sizeof(PIXELFORMATDESCRIPTOR),
+			1,
+			PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL | PFD_DOUBLEBUFFER,    //Flags
+			PFD_TYPE_RGBA,            //The kind of framebuffer. RGBA or palette.
+			32,                        //Colordepth of the framebuffer.
+			0, 0, 0, 0, 0, 0,
+			0,
+			0,
+			0,
+			0, 0, 0, 0,
+			24,                        //Number of bits for the depthbuffer
+			8,                        //Number of bits for the stencilbuffer
+			0,                        //Number of Aux buffers in the framebuffer.
+			PFD_MAIN_PLANE,
+			0,
+			0, 0, 0
+		};
+
+		HDC ourWindowHandleToDeviceContext = GetDC(hWnd);
+
+		int  letWindowsChooseThisPixelFormat;
+		letWindowsChooseThisPixelFormat = ChoosePixelFormat(ourWindowHandleToDeviceContext, &pfd);
+		SetPixelFormat(ourWindowHandleToDeviceContext, letWindowsChooseThisPixelFormat, &pfd);
+
+		HGLRC ourOpenGLRenderingContext = wglCreateContext(ourWindowHandleToDeviceContext);
+		wglMakeCurrent(ourWindowHandleToDeviceContext, ourOpenGLRenderingContext);
+
+		//glewExperimental = GL_TRUE;
+		int ginit = glewInit();
+		//MessageBoxA(0, glewGetErrorString(ginit), "OPENGL VERSION", 0);
+
+
+		//glViewport(0, 0, 100, 100);
+		//MessageBoxA(0, (char*)glGetString(GL_VERSION), "OPENGL VERSION", 0);
+		init();
+		//step();
+		//step();
+		//step();
+		//wglDeleteContext(ourOpenGLRenderingContext);
+		//PostQuitMessage(0);
+		ShowWindow(hWnd, SW_SHOW);
+	}
+	break;
+	case WM_CHAR:
+		switch (wParam)
+		{
+		case 'w':
+			looktilt += 0.1;
+			break;
+		case 's':
+			looktilt -= 0.1;
+			break;
+
+		case 'a':
+			lookrot += 0.1f;
+			break;
+		case 'd':
+			lookrot -= 0.1f;
+			break;
+		case 'f':
+			glTranslatef(0.1, 0, 0);
+			break;
+		case 'v':
+			glTranslatef(-0.1, 0, 0);
+			break;
+		case 'g':
+			glTranslatef(0,0.1, 0);
+			break;
+		case 'b':
+			glTranslatef(0, 0.1, 0);
+			break;
+		case 'h':
+			glTranslatef(0, 0, 0.1);
+			break;
+		case 'n':
+			glTranslatef(0,0,-0.1);
+			break;
+
+		}
+		break;
+	case WM_SIZE:
+	{
+		// resize the viewport after the window had been resized
+		//Resize(LOWORD(lparam), HIWORD(lparam));
+		width = LOWORD(lParam);
+		height = HIWORD(lParam);
+        resize();
+
+		break;
+	}
+	case WM_CLOSE:
+		DestroyWindow(wnd);
+		break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	case WM_MOUSEWHEEL:
+	{
+		int zDelta = GET_WHEEL_DELTA_WPARAM(wParam);
+		radius += (float)zDelta / 1200;
+		break;
+	}
+	default:
+		return DefWindowProc(hWnd, message, wParam, lParam);
+	}
+	return 0;
+
+}
